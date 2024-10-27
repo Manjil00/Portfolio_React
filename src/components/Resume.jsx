d2cb321f81752082221b00c2b8876fa6895cc152
ACER <210220@softwarica.edu.np>
2024-10-27 10:33:49 +0545

    resumechange

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,8 +7,8 @@ const Resume = () => {
 return (
     <div className="Resume  w-auto h-auto mt-10 text-sm lg:text-xl  bg-bgcolor">
     <a
-    href='./resume/Resume_Manjil_Maharjan.pdf'
-    download="Resume_Manjil_Maharjan.pdf">
+    href='./resume/ManjilMaharjan_Resume.pdf'
+    download="ManjilMaharjan_Resume.pdf">
     <button className='button-default flex justify-start items-center gap-3 p-2 rounded-2xl text-slate-500'>
     <RiDownloadLine />Download Resume</button>
     </a>

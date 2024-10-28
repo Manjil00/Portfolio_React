ed3b25b5600a201b907f18fbfc8ada67d87f1c0f
ACER <210220@softwarica.edu.np>
2024-10-28 12:22:10 +0545

    sizes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,40 +16,40 @@ return (
             <h1 className="title text-orange-500 font-sans text-xl md:text-4xl font-bold ml-9 md:ml-14 ">PROJECT SHOWCASE</h1>
 
             <div className="projects h-auto w-full flex flex-col justify-evenly items-center gap-5 mt-5 md:mt-10 p-2">
-                <div className="project1 rounded-xl bg-bgcolor h-auto w-full flex flex-row justify-between items-start">
-                    <div className="words mt-3 md:mt-10">
+                <div className="project1 rounded-xl bg-bgcolor h-auto w-full flex flex-row justify-between items-start p-3">
+                    <div className="words">
                     <h1 className="title text-white ml-5 font-sans text-lg md:text-2xl font-bold">Ecommerce website for Nursery Firm</h1>
                     <p className=' text-slate-500 ml-5 font-sans text-xs md:text-lg mt-3  break-words'>Gajendra Nursery's e-commerce site, built with the MERN stack, features user authentication, a shopping cart,admin dashboard, and a responsive design for browsing and purchasing plants and services. It also includes a blog and supports gardening service bookings.</p>
                     </div>
-                    <div className="imgpart  rounded-tr-xl rounded-br-xl h-full w-[50%]  bg-white flex flex-col justify-center items-center">
-                        <div className="upperimg flex ">
-                        <img src='../images/projectsimg/nursery1.png' className='h-[50%] w-[50%]' alt='pic'/>
-                        <img src='../images/projectsimg/nursery2.png' className='h-[50%] w-[50%] rounded-tr-xl' alt='pic'/>
+                    <div className="imgpart rounded-xl  h-[200px] md:h-auto w-[800px] p-3 gap-5 bg-white flex flex-col justify-center items-center">
+                        <div className="upperimg flex justify-between items-center gap-2">
+                        <img src='../images/projectsimg/nursery1.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                        <img src='../images/projectsimg/nursery2.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
                         </div>
-                    <div className="lowerimg flex">
-                    <img src='../images/projectsimg/pgmobile.png' className='h-[300px] w-[300px]' alt='pic'/>
-                    {/* <img src='../images/projectsimg/e-commerceaboutus.png' className='h-[50%] w-[50%] rounded-br-xl' alt='pic'/> */}
+                    <div className="lowerimg flex justify-between items-center gap-2">
+                    <img src='../images/projectsimg/e-commerceaboutus.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                    <img src='../images/projectsimg/e-commerceaboutus.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
                     </div>
                     </div>
                 </div>
 
-                <div className="project2 rounded-xl bg-bgcolor h-auto w-auto flex flex-row justify-between items-start">
-                <div className="imgpart  rounded-tl-xl rounded-bl-xl h-auto w-auto bg-white flex flex-col justify-center items-center">
-                        <div className="upperimg flex">
-                        <img src='../images/projectsimg/kerapp.png' className='h-[300px] w-[300px] rounded-tl-xl' alt='pic'/>
-                        {/* <img src='../images/projectsimg/adminpanel.png' className='h-[60%] w-[50%] hidden md:block' alt='pic'/> */}
-                        </div>
-                    <div className="part2img flex">
-                    <img src='../images/projectsimg/weather.png' className='h-[50%] w-[50%] rounded-bl-xl' alt='pic'/>
-                    <img src='../images/projectsimg/news.png' className='h-[50%] w-[50%]' alt='pic'/>
-                    </div>
-                    </div>
-
-                    <div className="words mt-3 md:mt-10">
+                <div className="project2 rounded-xl bg-bgcolor h-auto w-full flex flex-row justify-between items-start p-3">
+                    <div className="words">
                     <h1 className="title text-white ml-5 font-sans text-lg md:text-2xl font-bold">Websites/Webapps having API integration</h1>
-                    <p className=' text-slate-500 ml-5 font-sans text-xs md:text-lg mt-3 break-words'>I have developed several dynamic web applications that leverage real-time API integrations to deliver updated and interactive user experiences. These projects include a weather app, a news app, among others. Each application uses external APIs to fetch and display live data, ensuring the content remains relevant and up-to-date. For instance, the weather app integrates with the OpenWeatherMap API to provide users with current weather conditions, forecasts, and related information. By utilizing state management tools like Redux and integrating real-time APIs, my applications ensure smooth data flow, enabling users to interact with fresh content effortlessly.</p>
+                    <p className=' w-[200px] md:w-[1000px] text-slate-500 ml-5 font-sans text-xs md:text-lg mt-3  break-words'>I have developed several dynamic web applications that leverage real-time API integrations to deliver updated and interactive user experiences. These projects include a weather app, a news app, among others. Each application uses external APIs to fetch and display live data, ensuring the content remains relevant and up-to-date. For instance, the weather app integrates with the OpenWeatherMap API to provide users with current weather conditions, forecasts, and related information. By utilizing state management tools like Redux and integrating real-time APIs, my applications ensure smooth data flow, enabling users to interact with fresh content effortlessly.</p>
+                    </div>
+                    <div className="imgpart rounded-xl  h-[200px] md:h-auto w-[450px] p-3 gap-5 bg-white flex flex-col justify-center items-center">
+                        <div className="upperimg flex justify-between items-center gap-2">
+                        <img src='../images/projectsimg/adminpanel.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                        <img src='../images/projectsimg/news.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                        </div>
+                    <div className="lowerimg flex justify-between items-center gap-2">
+                    <img src='../images/projectsimg/weather.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                    <img src='../images/projectsimg/e-commerceaboutus.png' className='h-[50px] w-[50px] md:h-[50%] md:w-[50%]' alt='pic'/>
+                    </div>
                     </div>
                 </div>
+
 
             </div>
         </div>
@@ -61,3 +61,6 @@ return (
 }
 
 export default Projects;
+
+
+
